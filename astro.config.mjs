bdcf757878660cc9f92b334fdd186a7f79176914
narcisolobo <narcisolobo@gmail.com>
2025-05-21 09:19:34 -0700

    installing decap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  env: {
+    schema: {
+      PUBLIC_GITHUB_CLIENT_ID: envField.string({
+        context: "client",
+        access: "public",
+      }),
+      GITHUB_CLIENT_SECRET: envField.string({
+        context: "server",
+        access: "secret",
+      }),
+    },
+  },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
